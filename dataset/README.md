91307b59a9af24ebc1ac5d56f6448b83085cfeb9
Akram Sbaih <akram@stanford.edu>
2021-02-25 15:58:58 -0800

    Add script for download and preperation of dataset for traj-transformer format

--- dataset/README.md
+++ dataset/README.md
@@ -1,19 +1,20 @@
 ## How to get the data
 This directory helps you download and prepare the [UZH FPV Dataset](https://fpv.ifi.uzh.ch/datasets/).
 * `dataset_links.txt` are the links to download all the Leica zip files listed [here](https://fpv.ifi.uzh.ch/datasets/) as of Feb 24, 2021.
-* `download_preprocess.py` takes the previous list, downloads it, unzips it, and runs a script adapted from [the dataseet toolkit](https://github.com/uzh-rpg/uzh_fpv_open) which converts it to the following format and stores it in `data/` directory.
+* `download_preprocess.py` takes the previous list, downloads it, unzips it, and formats it to a provided `FPS` and stores it locally.
+* `uzh_fpv_utils.py` is a script adapted from [the dataseet toolkit](https://github.com/uzh-rpg/uzh_fpv_open) to parse `Leica` format.
 
 ## Data format
 After following the previous steps, you'll have the following structure:
 ```
 data -
-     |- t   - 
-            |- <env name>_<id>.npy
-            |- <env name>_<id>.npy
-            ...
-     |- xyz - 
-            |- <env name>_<id>.npy
-            |- <env name>_<id>.npy
+     |-train- 
+            |- <env name>_<id>.txt
+            |- <env name>_<id>.txt
             ...
 ```
-For each file, `<env name>` and `<env id>` match the naming in the original dataset and they describe the different data collection runs performed in different environments and different routes and velocities. The files under `t` are the timestamps. For each of them, there's a file with the same name under `xyz` and they map one-to-one to the absolute location of the drone at that timestamp.  
+For each file, `<env name>` and `<env id>` match the naming in the original dataset and they describe the different data collection runs performed in different environments and different routes and velocities. 
+
+The file follows the dataset format for the baseline [Trajectory Transformer](https://github.com/FGiuliari/Trajectory-Transformer). Each row is a frame in a constant `FPS` chosen at [download_preprocess.py](download_preprocess.py). The columns are `frame_idx,drone_idx,x,y,z` and are tab-separated. 
+
+If you wish to have a test set, you can move some of the data files to a separate test set directory. 
--- dataset/download_preprocess.py
+++ dataset/download_preprocess.py
@@ -1,116 +1,22 @@
 """
-This code is adopted from the github.com/uzh-rpg/uzh_fpv_open repo with code added for download convenience by
-Akram Sbaih (akram at stanford dot edu).
+Copyright (C) 2021 Akram Sbaih, Stanford University
+    You can contact the author at <akram at stanford dot edu>
+
+This script helps you download and process the UZH FPV dataset
+described at https://github.com/uzh-rpg/uzh_fpv_open
 """
 
-# Copyright (C) 2020 Titus Cieslewski, RPG, University of Zurich, Switzerland
-#   You can contact the author at <titus at ifi dot uzh dot ch>
-# Copyright (C) 2020 Davide Scaramuzza, RPG, University of Zurich, Switzerland
-#
-# This file is part of uzh_fpv_open.
-#
-# uzh_fpv_open is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# uzh_fpv_open is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with uzh_fpv_open. If not, see <http:#www.gnu.org/licenses/>.
-
-import datetime
 import numpy as np
-import re
+import pandas as pd
 import os
-import time
-import math
 import wget
 import zipfile
 from tqdm import tqdm
-
-class GeneralizedTime(object):
-    """ ROS time's typing and positivity check is too restrictive. """
-    def __init__(self, secs_or_time_obj, nsecs=None):
-        """ Secs and nsecs may be negative. """
-        if hasattr(secs_or_time_obj, 'secs'):
-            assert hasattr(secs_or_time_obj, 'nsecs')
-            self.__init__(secs_or_time_obj.secs, secs_or_time_obj.nsecs)
-        elif type(secs_or_time_obj) == float:
-            dec, i = math.modf(secs_or_time_obj)
-            self.__init__(int(i), int(dec * 1e9))
-        else:
-            assert nsecs is not None
-            assert abs(nsecs) < 1e9
-            self.secs = secs_or_time_obj
-            self.nsecs = nsecs
-
-    def to_sec(self):
-        return self.secs + float(self.nsecs) / 1e9
-
-    def __add__(self, other):
-        secs = self.secs + other.secs
-        nsecs = self.nsecs + other.nsecs
-        if abs(nsecs) >= 1e9:
-            if nsecs > 0:
-                nsecs = nsecs - 1e9
-                secs = secs + 1
-            else:
-                nsecs = nsecs + 1e9
-                secs = secs - 1
-        return GeneralizedTime(secs, nsecs)
-
-    def __sub__(self, other):
-        return self.__add__(-GeneralizedTime(other))
-
-    def __neg__(self):
-        return GeneralizedTime(-self.secs, -self.nsecs)
-
-def parseTextFile(path):
-    leica_file = open(path, 'r')
-
-    # Patterns:
-    date_time = '\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d.\d\d\d'
-    value = '-?\d+\.\d+'
-
-    op_line = '^0,%s,Operator,.*$' % date_time
-    empty_line = '^[\r\n]?$'
-    start_line = '^0,%s,Start![\r\n]?$' % date_time
-    weird_line = '^1,%s,%%R1Q,0,\d:,%s,%%R1P,0,\d:0[\r\n]?$' % (date_time, date_time)
-    meas_line = re.compile('^3,(%s),%%R1Q,2082,\d:10000,1,%s,%%R1P,0,\d:(\d+),(%s),(%s),(%s),\d+,%s,%s,%s,\d+[\r\n]?$'
-                           % (date_time, date_time, value, value, value, value, value, value))
-    outlier_line = '^3,%s,%%R1Q,2082,\d:10000,1,%s,%%R1P,0,\d:\d+,0,0,0,0,0,0,0,0[\r\n]?$' % (
-        date_time, date_time)
-    final_line = '^0,%s,Canceled![\r\n]?$' % date_time
-
-    t = []
-    x = []
-    y = []
-    z = []
-    for line in leica_file:
-        meas_match = meas_line.match(line)
-        if meas_match:
-            if meas_match.group(2) in ['0', '1284']:
-                dt = datetime.datetime.strptime(meas_match.group(1) + '000', '%Y-%m-%d %H:%M:%S.%f')
-                # Could also use ROS Time here, or any other time representation.
-                t.append(GeneralizedTime(int(time.mktime(dt.timetuple())), dt.microsecond * 1000))
-                x.append(float(meas_match.group(3)))
-                y.append(float(meas_match.group(4)))
-                z.append(float(meas_match.group(5)))
-        elif re.match(op_line, line) or re.match(empty_line, line) or re.match(start_line, line) or \
-                re.match(weird_line, line) or re.match(final_line, line) or re.match(outlier_line, line):
-            pass
-        else:
-            print('The following line does not match any pattern:')
-            print(line)
-
-    return t, np.array([x, y, z]).T
+from .uzh_fpv_utils import parseTextFile
 
 if __name__=="__main__":
     # download the zip files listed in dataset_links.txt
+    # if you already have them downloaded, you can comment out the following lines
     print("Downloading zips...")
     links = [line.strip() for line in open("dataset_links.txt", 'r').readlines() if line and line[0] != '#']
     zips = []
@@ -118,6 +24,7 @@ if __name__=="__main__":
     for link in tqdm(links):
         zips.append("zips/" + link.split('/')[-1])
         wget.download(link, zips[-1])
+    zips = ['zips/'+file for file in os.listdir('zips') if file.endswith('.zip')]
     # unzip files
     print("Unzipping...")
     unzips = []
@@ -129,11 +36,22 @@ if __name__=="__main__":
     # parse files and store as np files
     print("Preprocessing...")
     if not os.path.exists('data'): os.mkdir('data')
-    if not os.path.exists('data/t'): os.mkdir('data/t')
-    if not os.path.exists('data/xyz'): os.mkdir('data/xyz')
+    if not os.path.exists('data/train'): os.mkdir('data/train')
     for unzip in tqdm(unzips):
         identifier = unzip.split('/')[-1]
-        t, xyz = parseTextFile(unzip+'/leica.txt')
-        np.save('data/t/'+identifier, t)
-        np.save('data/xyz/'+identifier, xyz)
+        timestamps, xyz = parseTextFile(unzip+'/leica.txt')
+        # start timestamps at 0 instead
+        timestamps = np.array([ts.to_sec() for ts in timestamps]) - timestamps[0].to_sec()
+        file_duration = timestamps[-1]
+        # new timestamps that follow a constant FPS
+        FPS = 8
+        desired_timestamps = np.linspace(0, file_duration, int(FPS * file_duration))
+        # transform to constant FPS and then to integer frame index
+        df = pd.DataFrame(xyz, index=timestamps)
+        df = df[~df.index.duplicated(keep='first')]\
+            .reindex(desired_timestamps, method='nearest')
+        df.index = list(range(int(file_duration * FPS)))
+        # insert a pedastrian column with a constant value... this could be used to reflect having multiple drones
+        df.insert(0, "ped", 1.)
+        df.to_csv("data/train/"+identifier+".txt", sep="\t", header=False)
     print("Done!")
